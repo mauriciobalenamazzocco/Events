2b47c42c5e13b7c55f29aadc2c5426866bf4d679
Mauricio Balena Mazzocco <mauricio.balena@fluig.com>
2020-08-29 17:33:27 -0300

    Initial project

---
+++ Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Mauricio Balena Mazzocco on 29/08/20.
+//  Copyright © 2020 Mauricio Mazzocco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
