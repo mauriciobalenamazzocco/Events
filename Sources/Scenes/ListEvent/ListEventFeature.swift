8056d8cd5499c6cc4cf2edf19f372292148eb4d7
Mauricio Balena Mazzocco <mauricio.balena@fluig.com>
2020-08-30 18:31:33 -0300

    ListEventFeature manage states to load or refresh Events

---
+++ Sources/Scenes/ListEvent/ListEventFeature.swift
@@ -0,0 +1,60 @@
+import Foundation
+import RxSwift
+import SpinCommon
+import SpinRxSwift
+
+class ListEventFeature: ListFeature<Occurrence> {
+    typealias ListFeatureType = ListFeature<Occurrence>
+
+    private init(eventApi: EventAPIProtocol = EventAPI()) {
+        let refresh = partial(ListEventFeature.refresh, arg1: eventApi, arg2: .partial)
+        let loadPage = partial(ListEventFeature.loadPage, arg1: eventApi, arg2: .partial)
+
+        let spinner = Spinner
+            .initialState(ListFeatureType.State.idle)
+            .feedback(Feedback(effect: refresh))
+            .feedback(Feedback(effect: loadPage))
+            .reducer(Reducer(ListFeatureType.reduce))
+
+        super.init(spin: spinner)
+    }
+
+    static func refresh(eventAPI: EventAPIProtocol,
+                        state: ListFeatureType.State) -> Observable<ListFeatureType.Event> {
+        guard case .refreshing(_, _) = state else { return .empty() }
+
+        return Single.create { single in
+
+            eventAPI.fetchAllEvents { result in
+                switch result {
+                case let .success(events):
+                    let listPage = ListPage<Occurrence>.first(items: events, hasNext: false)
+                    single(.success(.refreshed(listPage)))
+                case let .failure(error):
+                    single(.success(.errored(error)))
+                }
+            }
+            return Disposables.create()
+        }.asObservable()
+    }
+
+    static func loadPage(eventAPI: EventAPIProtocol,
+                         state: ListFeatureType.State) -> Observable<ListFeatureType.Event> {
+        guard case .loadingPage(_, _) = state else { return .empty() } //Used to pagination and Filter, but this api cant do a pagination
+
+        return Single.create { single in
+
+            eventAPI.fetchAllEvents { result in
+                switch result {
+                case let .success(events):
+                    let listPage = ListPage<Occurrence>.first(items: events, hasNext: false)
+                    single(.success(.pageLoaded(listPage)))
+                case let .failure(error):
+                    single(.success(.errored(error)))
+                }
+            }
+            return Disposables.create()
+        }.asObservable()
+
+    }
+}
