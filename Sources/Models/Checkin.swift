946f583a5b78b7cf398be71ff442b24217fc594b
Mauricio Balena Mazzocco <mauricio.balena@fluig.com>
2020-08-31 15:26:34 -0300

    Create checkin models to use on CheckinAPI

---
+++ Sources/Models/Checkin.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct Checkin: Codable {
+    var eventId: String
+    var name: String
+    var email: String
+}
---
+++ Sources/Models/CheckinResponse.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct CheckinResponse: Codable {
+    var code: String?
+}
