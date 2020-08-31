0baa504f8246ff12e7f6ec0dcad321cedd6072d4
Mauricio Balena Mazzocco <mauricio.balena@fluig.com>
2020-08-30 22:07:55 -0300

    Update Event app add fetchEventDetail to get Detail of Specific Event

--- Sources/Clients/EventAPI.swift
+++ Sources/Clients/EventAPI.swift
@@ -4,8 +4,10 @@ import RxCocoa
 
 protocol EventAPIProtocol {
     typealias EventsResponse = Result<[Occurrence], ServiceError>
+    typealias EventResponse = Result<Occurrence, ServiceError>
 
     func fetchAllEvents(completionHandler: @escaping (EventsResponse) -> Void)
+    func fetchEventDetail(id: String, completionHandler: @escaping (EventResponse) -> Void)
 }
 
 struct EventAPI: EventAPIProtocol {
@@ -37,8 +39,34 @@ struct EventAPI: EventAPIProtocol {
         }
         dataTask.resume()
     }
+
+    func fetchEventDetail(id: String,
+                          completionHandler: @escaping (EventResponse) -> Void ) {
+
+        guard let url = URL(string: String(format: EventAPI.apiEventPath, id)) else {
+            completionHandler(.failure(.urlInvalid))
+            return
+        }
+
+        let dataTask = urlSession.dataTask(with: URLRequest(url: url)) { (data, _, error) in
+            if let error = error {
+                completionHandler(.failure(.api(error)))
+            } else if let data = data {
+                guard let ocurrences = try? JSONDecoder().decode(Occurrence.self, from: data) else {
+                    completionHandler(.failure(.parse))
+                    return
+                }
+
+                completionHandler(.success(ocurrences))
+            }
+        }
+        dataTask.resume()
+    }
+
 }
 
 extension EventAPI {
-    static let apiEventPath = "http://5b840ba5db24a100142dcd8c.mockapi.io/api/events" //This api is not pageable =(
+    static let api = "http://5b840ba5db24a100142dcd8c.mockapi.io/api"
+    static let apiEventPath = "\(api)/events" //This api is not pageable =(
+    static let apiEventDetailPath = "\(api)/events/%@"
 }
